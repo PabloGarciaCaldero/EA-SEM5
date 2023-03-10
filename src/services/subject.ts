bbc417b37447eba0d4b5b15da4e776c2e2f49adc
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-03-10 17:54:52 +0100

    Primera funció extra feta

---
+++ src/services/subject.ts
@@ -0,0 +1,68 @@
+import { Types } from "mongoose";
+import { Subject } from "../interfaces/subject.interface";
+import SubjectModel from "../models/subject";
+import UserModel from "../models/user";
+
+const insertSubject=async(item:Subject)=>{
+    const responseInsert=await SubjectModel.create(item);
+    return responseInsert;
+};
+
+const getSubjects=async()=>{
+    const responseItem=await SubjectModel.find({}).populate('users');
+    return responseItem;
+};
+
+const getSubject=async(id:string)=>{
+    const responseItem=await SubjectModel.findOne({_id:id}).populate('users');
+    return responseItem;
+};
+
+const updateSubject=async(id:string,data:Subject)=>{
+    const responseItem=await SubjectModel.findOneAndUpdate(
+        {_id:id},
+        data,
+        {
+            new:true,
+        }
+    ).populate('users');
+    return responseItem;
+};
+
+const deleteSubject=async(id:string)=>{
+    const responseItem=await SubjectModel.deleteOne({_id:id});
+    return responseItem;
+}
+
+const matriculateSubject=async(idUser:string,idSubject:string)=>{
+    const responseItem = await SubjectModel.findOneAndUpdate(
+        {_id:idSubject},
+        {$addToSet: {users: new Types.ObjectId(idUser)}},
+        {new: true}
+    ).populate('users');
+    console.log(responseItem?.users);
+    return responseItem;
+};
+/*const subjectsOfAUser = async (idUser:string,idSubject:string) => {
+    const responseItem = await SubjectModel.findOne(
+        {_id:idSubject},
+        {}
+    )
+}*/
+
+const getUsersInSubject = async (idSubject:string) => {
+    //busco la asignatura en concreto
+    const SpecificSubject = await SubjectModel.findOne({_idSubject:idSubject});
+    const studentsIds = SpecificSubject?.users ?? [];
+    const users = await UserModel.find({
+        _id: { $in: studentsIds }
+      });
+      return users;
+
+}
+
+
+
+
+
+export { insertSubject, getSubject, getSubjects, updateSubject, deleteSubject, matriculateSubject, getUsersInSubject };
